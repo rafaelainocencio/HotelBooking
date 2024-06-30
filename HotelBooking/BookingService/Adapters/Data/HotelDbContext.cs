f3913e46f30949fa127c0e931a3400b4c9830b0e
rafaela montalvao <rafaelainocencioo@gmail.com>
2024-06-30 17:57:57 -0300

    feat(database): add migrations for foreign key relationships and update schema
    - Added foreign key relationships for Booking entity:
      - Added GuestId and RoomId columns to Bookings table
      - Created indexes and foreign key constraints for GuestId and RoomId
    
    - Updated database schema with new migration:
      - Migration: AddingForeignToBooking
      - Updated DbContext to include new relationships

--- HotelBooking/BookingService/Adapters/Data/HotelDbContext.cs
+++ HotelBooking/BookingService/Adapters/Data/HotelDbContext.cs
@@ -8,5 +8,7 @@ namespace Data
         public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options) { }
 
         public virtual DbSet<Guest> Guests { get; set; }
+        public virtual DbSet<Room> Rooms { get; set; }
+        public virtual DbSet<Booking> Bookings { get; set; }
     }
 }

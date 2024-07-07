71d79a7ae8af83d6b69d8720c3017d42ab7fa047
rafaela montalvao <rafaelainocencioo@gmail.com>
2024-07-07 14:14:42 -0300

    feat(domain): add value objects and corresponding migrations
    - Added value objects to Room and Guest entities:
      - Room entity now includes a Price value object
      - Guest entity now includes a DocumentId value object
    
    - Created migrations to reflect the schema changes:
      - Migration: AddingValueObjectsToRoom
      - Migration: AddingValueObjectToGuest

---
+++ HotelBooking/BookingService/Adapters/Data/GuestConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class GuestConfiguration : IEntityTypeConfiguration<Guest>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Guest> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.OwnsOne(x => x.DocumentId)
+                .Property(x => x.IdNumber);
+
+            builder.OwnsOne(x => x.DocumentId)
+                .Property(x => x.DocumentType);
+        }
+    }
+}
--- HotelBooking/BookingService/Core/Domain/Entities/Guest.cs
+++ HotelBooking/BookingService/Core/Domain/Entities/Guest.cs
@@ -1,4 +1,6 @@
-﻿namespace Domain.Entities
+﻿using Domain.ValueObjects;
+
+namespace Domain.Entities
 {
     public class Guest
     {
@@ -6,5 +8,8 @@
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
+        //não queremos que isso vire uma tabela
+        //mas que os atributos do obj se tornem campos da tb do Guest
+        public PersonId DocumentId { get; set; } 
     }
 }
